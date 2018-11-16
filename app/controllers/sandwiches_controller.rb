fbbf39657a4ead4afc3abfb692cf4d57fec20520
ValerieHMoore <periwinkle17@outlook.com>
2018-11-16 14:36:53 -0500

    add sandwich_params

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -1,2 +1,13 @@
 class SandwichesController < ApplicationController
+
+    def new
+        @sandwich = Sandwich.new
+    end
+
+    private
+
+    def sandwich_params
+        params.require(:name).permit(:toast, :grill, :open_face)
+    end
+    
 end
