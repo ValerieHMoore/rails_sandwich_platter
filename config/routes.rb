653ec8ec8bed2bf3db215006d154e5287a3f8e87
ValerieHMoore <periwinkle17@outlook.com>
2018-11-17 12:35:27 -0500

    change nested show routes to new to meet project requirements

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :sandwiches do
-    resources :greens, only: [:show, :index]
-    resources :cheeses, only: [:show, :index]
-    resources :meats, only: [:show, :index]
-    resources :spreads, only: [:show, :index]
-    resources :breads, only: [:show, :index]
+    resources :greens, only: [:new, :index]
+    resources :cheeses, only: [:new, :index]
+    resources :meats, only: [:new, :index]
+    resources :spreads, only: [:new, :index]
+    resources :breads, only: [:new, :index]
   end
   
   resources :sandwiches
