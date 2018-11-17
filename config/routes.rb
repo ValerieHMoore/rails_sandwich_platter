94f013fb46cb319fb65193a127fa30f7cb434c99
ValerieHMoore <periwinkle17@outlook.com>
2018-11-17 12:31:02 -0500

    create nested resources for sandwich contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :sandwiches
-  resources :greens
-  resources :cheeses
-  resources :meats
-  resources :spreads
-  resources :breads
-  resources :eaters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :sandwiches do
+    resources :greens, only: [:show, :index]
+    resources :cheeses, only: [:show, :index]
+    resources :meats, only: [:show, :index]
+    resources :spreads, only: [:show, :index]
+    resources :breads, only: [:show, :index]
+  end
+  
+  resources :sandwiches
+  resources :eaters
+
 end
