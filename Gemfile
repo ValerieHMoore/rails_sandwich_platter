2931ff5a131390ec3e3b08f766a35e5ab4c925b5
ValerieHMoore <periwinkle17@outlook.com>
2018-11-19 11:44:03 -0500

    gem update

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.3.3'
 
 gem 'pry'
-gem 'bcrypt', '~> 3.1', '>= 3.1.11'
+gem 'bcrypt'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@ CSV.foreach(Rails.root.join("breads.csv"), headers: true) do |row|
   Bread.create! do |bread|
     bread.id = row[0]
     bread.name = row[1]
- . end
+  end
 end
 
 puts "Importing spreads..."
 CSV.foreach(Rails.root.join("spreads.csv"), headers: true) do |row|
   Spread.create! do |spread|
-    spread.name = row[0]
-    spread_id = row[2]
+    spread.id = row[0]
+    spread.name = row[1]
   end
 end
+
+end
+
+# csv_text = File.read(__dir__ + "/Sandwiches3.csv")
+# csv = CSV.parse(csv_text, :headers => true)
+# csv.each do |row|  
+# Sandwich.create(sandwich_name: row.to_hash["﻿sandwich_name"], ingredients: row.to_hash["ingredients"]) 
+# end
